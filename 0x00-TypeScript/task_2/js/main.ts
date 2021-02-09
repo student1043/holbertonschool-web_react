b0aee7ce7684802b0436ff05db6bf36c5216e970
student1043 <1043@holbertonschool.com>
2021-02-09 23:14:01 +0100

    Task 5

---
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -0,0 +1,44 @@
+interface DirectorInterface {
+    workFromHome: () => string;
+    getCoffeeBreak: () => string;
+    workDirectorTasks: () => string;
+}
+
+interface TeacherInterface {
+    workFromHome: () => string;
+    getCoffeeBreak: () => string;
+    workDirectorTasks: () => string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome() {
+        return "Working from home";
+    }
+    getCoffeeBreak() {
+        return "Getting a coffee break";
+    }
+    workDirectorTasks() {
+        return "Getting to director tasks";
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome() {
+        return "Cannot work from home";
+    }
+    getCoffeeBreak() {
+        return "Cannot have a break";
+    }
+    workDirectorTasks() {
+        return "Getting to work";
+    }
+}
+
+function createEmployee(salary: any) {
+    if (typeof(salary) == 'number' && salary < 500) {
+        return new Teacher;
+    }
+    else {
+        return new Director;
+    }
+}
