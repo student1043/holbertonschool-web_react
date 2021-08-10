51e033b52ece76494c101767549083881590c964
student1043 <1043@holbertonschool.com>
2021-08-10 16:29:35 +0200

    Task 1

---
+++ 0x02-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Notifications.css'
+
+export default Notifications = () =>  {
+    return (
+        <div className="Notifications">
+            <p>Here is the list of notifications</p>
+        </div>
+    )
+}
