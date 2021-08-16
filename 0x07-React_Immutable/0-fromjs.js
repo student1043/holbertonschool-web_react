573e4da7e8abe0e24e6859daec0f9156c0565ea5
student1043 <1043@holbertonschool.com>
2021-08-16 16:43:17 +0200

    Update 0-fromjs.js

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,3 +1,5 @@
 import { fromJS } from 'immutable';
 
-export const getImmutableObject = (object) => fromJS(object);
+const getImmutableObject = (object) => fromJS(object);
+
+export default getImmutableObject;
