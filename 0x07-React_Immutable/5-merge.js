731208f5c842538d9a32b9ffc6cd772f1337fbe0
student1043 <1043@holbertonschool.com>
2021-08-16 19:48:19 +0200

    Create 5-merge.js

---
+++ 0x07-React_Immutable/5-merge.js
@@ -0,0 +1,9 @@
+import { Map, List } from 'immutable';
+
+export function concatElements (page1, page2) {
+  return List(page1.concat(page2));
+}
+
+export function mergeElements (page1, page2) {
+  return Map(page1.merge(page2));
+}
