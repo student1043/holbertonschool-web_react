d6d9eb5a9ac8192a438e5549b785ef7b4e008dd7
student1043 <1043@holbertonschool.com>
2021-08-16 18:24:22 +0200

    Update 2-nested.js

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,7 +1,7 @@
-import { Map } from 'immutable';
+import { fromJS } from 'immutable';
 
 const accessImmutableObject = (object, array) => {
-    return Map(object).getIn(array, undefined);
+    return fromJS(object).getIn(array, undefined);
 }
 
 export default accessImmutableObject;
