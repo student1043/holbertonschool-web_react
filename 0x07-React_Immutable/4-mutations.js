9dd6c2504cb7fddb670a06d7dde2f1f84f453390
student1043 <1043@holbertonschool.com>
2021-08-16 19:40:29 +0200

    Update 4-mutations.js

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -1,13 +1,16 @@
-import { Map } from 'immutable'
+import { Map } from 'immutable';
 
-export const map = Map({
-    1: 'Liam',
-    2: 'Noah',
-    3: 'Elijah',
-    4: 'Oliver',
-    5: 'Jacob',
-    6: 'Lucas',
-  });
+export const map = [
+    ['1', 'Liam'],
+    ['2', 'Noah'],
+    ['3', 'Elijah'],
+    ['4', 'Oliver'],
+    ['5', 'Jacob'],
+    ['6', 'Lucas']
+  ];
 
-export const map2 = map.getIn(['2', 'Benjamin']);
-map2 = map.getIn(['4', 'Oliver']);
+map = Map(map);
+
+export const map2 = map.withMutations((mapItem) => {
+  mapItem.set(2, 'Benjamin').set(4, 'Oliver');
+});
