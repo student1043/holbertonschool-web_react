01159cfd35f16ddc6a9dccc652e4e2d1bb13b6e1
student1043 <1043@holbertonschool.com>
2021-08-16 19:53:37 +0200

    Update 5-merge.js

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,9 +1,13 @@
 import { Map, List } from 'immutable';
 
 export function concatElements (page1, page2) {
-  return List(page1.concat(page2));
+  const pageone = List(page1);
+  const pagetwo = List(page2);
+  return pageone.concat(pagetwo);
 }
 
 export function mergeElements (page1, page2) {
-  return Map(page1.merge(page2));
+  const pageone = List(page1);
+  const pagetwo = List(page2);
+  return pageone.merge(pagetwo);
 }
