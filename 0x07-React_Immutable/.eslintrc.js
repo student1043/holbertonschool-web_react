a65e28842e30ccb1275c5dc9c2361bf8cfda1090
student1043 <1043@holbertonschool.com>
2021-08-17 12:50:17 +0200

    add

--- 0x07-React_Immutable/.eslintrc.js
+++ 0x07-React_Immutable/.eslintrc.js
@@ -1,25 +1,36 @@
+
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
-  rules: {
-  }
-}
+	env: {
+	  browser: false,
+	  es6: true,
+	  jest: true,
+	},
+	extends: [
+	  'airbnb-base',
+	  'plugin:jest/all',
+	],
+	globals: {
+	  Atomics: 'readonly',
+	  SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+	  ecmaVersion: 2018,
+	  sourceType: 'module',
+	},
+	plugins: ['jest'],
+	rules: {
+	  'no-console': 'off',
+	  'no-shadow': 'off',
+	  'no-restricted-syntax': [
+		'error',
+		'LabeledStatement',
+		'WithStatement',
+	  ],
+	},
+	overrides:[
+	  {
+		files: ['*.js'],
+		excludedFiles: 'babel.config.js',
+	  }
+	]
+  };
--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,13 +1,13 @@
 import { Map, List } from 'immutable';
 
-export function concatElements (page1, page2) {
-    const pageone = List(page1);
-    const pagetwo = List(page2);
-    return pageone.concat(pagetwo);
+export function concatElements(page1, page2) {
+  const pageone = List(page1);
+  const pagetwo = List(page2);
+  return pageone.concat(pagetwo);
 }
 
-export function mergeElements (page1, page2) {
-    const pageone = Map(page1);
-    const pagetwo = Map(page2);
-    return pageone.merge(pagetwo);
+export function mergeElements(page1, page2) {
+  const pageone = Map(page1);
+  const pagetwo = Map(page2);
+  return pageone.merge(pagetwo);
 }
