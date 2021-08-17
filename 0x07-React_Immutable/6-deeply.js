1bd04f0987d46e8a6e2d41957c6fff5b2fef82f7
student1043 <1043@holbertonschool.com>
2021-08-17 15:01:22 +0200

    Update 6-deeply.js

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -1,5 +1,7 @@
 import { Map } from 'immutable';
 
-export function mergeDeeplyElements(page1, page2) {
-    return(Map(page1).mergeDeep(Map(page2)));
+export default function mergeDeeplyElements(page1, page2) {
+  const pageone = Map(page1);
+  const pagetwo = Map(page2);
+  return pageone.mergeDeep(pagetwo);
 }
