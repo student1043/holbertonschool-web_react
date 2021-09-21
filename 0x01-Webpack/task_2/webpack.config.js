d86ad0d7711da517056775e50ef1098c6b1d2864
student1043 <55001682+student1043@users.noreply.github.com>
2021-09-21 18:26:36 +0100

    task 2

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./js/dashboard_main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: ['file-loader', 'image-webpack-loader']
+      },
+    ]
+  },
+}
